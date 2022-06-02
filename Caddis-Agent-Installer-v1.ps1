b4457c6d99e33b13eac883c20cc70689f7f89995
addictivepixels <addictivepixels@mac.com>
2022-06-02 09:01:36 -0400

    Add files via upload

---
+++ Caddis-Agent-Installer-v1.ps1
@@ -0,0 +1,25 @@
+# This will force PowerShell to use TLS 1.2 just in case TLS 1.0 or 1.1 is active, and we know how bad that is :)
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
+
+#Setup a temp folders just in-case
+New-Item -Path 'C:\temp' -ItemType Directory -force
+New-Item -Path 'C:\temp\caddis' -ItemType Directory -force
+
+# Delete any previous Caddis zips
+Remove-Item 'C:\temp\caddis\caddis-agent-v1.zip' -Force -WarningAction SilentlyContinue
+
+# Download Agent
+$Url = 
+Invoke-WebRequest -Uri $Url -OutFile $ZipFile 
+
+# Unzip and tidy up
+Expand-Archive -LiteralPath 'C:\temp\caddis-agent-v1.zip' -DestinationPath 'C:\temp\caddis\' -Force
+Remove-Item 'C:\temp\caddis-agent-v1.zip' -Force -WarningAction SilentlyContinue
+
+# Install agent
+msiexec.exe /i "C:\temp\caddis\Setup-Windows-SilentMSI.msi" /qn
+
+# Sleep for little bit to ensure subprocesses can do their thing...
+sleep 10
+
+exit
